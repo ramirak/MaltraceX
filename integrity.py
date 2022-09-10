be12c903f57aec5de3183d482296ae5ecec59316
ramirak <ramirak123@gmail.com>
2022-09-10 13:32:42 +0300

    Initial Commit

---
+++ integrity.py
@@ -0,0 +1,53 @@
+import glob
+import hashlib
+import os
+import data
+
+def take_snapshot(path):
+    sys_map = {}
+    for filename in glob.iglob(path + "**", recursive=True):
+        print(filename)
+        if os.path.isfile(filename):
+            sys_map.update({filename: sha256sum(filename)})
+    print("\nDone.\n")
+    return sys_map
+
+
+def check_integrity(sys_map_before, path):
+    f = open("traces.mt", "w")
+    f.write("----------------------------------------------------------------------------------\n")
+    f.write("--------------------------------Maltrace Log File---------------------------------\n")
+    f.write("----------------------------------------------------------------------------------\n")
+    if not bool(sys_map_before):
+        print("\nNo snapshot found\n")
+        return
+    for filename in glob.iglob(path + '**', recursive=True):
+        if os.path.isfile(filename):
+            if(filename not in sys_map_before):
+                f.write("\nFound new trace: " + filename + "\n")
+                f.write("\n----------------------------------------------------------------------------------\n")
+            if(filename in sys_map_before):
+                hash_before = sys_map_before[filename]
+                hash_after = sha256sum(filename)
+                if(hash_before != hash_after):
+                    f.write("\nFile - " + filename + " has changed.\n")
+                    f.write("Original hash : " + hash_before + "\n")
+                    f.write("Changed hash : " + hash_after + "\n")
+                    f.write("\n----------------------------------------------------------------------------------\n")
+    f.close()
+    data.show_traces()
+
+                    
+def sha256sum(filename):
+    if os.path.isdir(filename):
+        return;
+    h  = hashlib.sha256()
+    b  = bytearray(128*1024)
+    mv = memoryview(b)
+    try:
+        with open(filename, 'rb', buffering=0) as f:
+            while n := f.readinto(mv):
+                h.update(mv[:n])
+    except:
+        return -1
+    return h.hexdigest()
---
+++ maltrace.py
@@ -0,0 +1,45 @@
+import os
+import data, integrity
+
+hashes_file = "hashes.mt"
+conf_file = "maltrace.conf"
+
+def init():
+    with open('logo.txt', 'r') as f:
+        print(f.read())
+    while(True):
+        main_menu()
+   
+
+def main_menu():
+    sys_map = {}
+    conf = data.retrieve_from_file(conf_file)
+    path = conf["path"]
+    if os.path.exists(hashes_file):
+        sys_map = data.retrieve_from_file(hashes_file)
+    choice = -1
+    while(choice == -1):
+        print("(1) Take system Snapshot")
+        print("(2) Test system integrity")
+        print("(3) Exit")
+        choice = assert_choice(input("\n> "))
+        if(choice == 1):
+            sys_map = integrity.take_snapshot(path)
+            data.dump_to_file(sys_map, hashes_file)
+        elif(choice == 2):
+            integrity.check_integrity(sys_map, path)
+        elif(choice == 3):
+            exit(0)
+        else:
+            choice = -1
+
+
+def assert_choice(choice):
+    try:
+        choice = int(choice)
+    except:
+        return -1;
+    return choice
+
+
+init()
